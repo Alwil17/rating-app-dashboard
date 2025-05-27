0055eeb633a7d4d7cd59f1c5b06f44342bea481e
alwil17 <willialfred24@gmail.com>
2025-05-27 09:19:02 +0000

    feat: enhance AdminLayout with breadcrumb navigation and responsive design elements

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -8,8 +8,13 @@ import { routes } from '@/config/routes';
 import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { LogOut, User } from 'lucide-react';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/layout/app-sidebar';
+import { Separator } from '@radix-ui/react-separator';
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from '@/components/ui/breadcrumb';
+import { PiGauge } from "react-icons/pi";
+import UserDropdown from '@/components/layout/user.dropdown';
+import { ModeToggle } from '@/components/layout/mode-toggle';
 
 function AdminLayout({ children }: { children: React.ReactNode }) {
   const { user } = useAuth();
@@ -27,10 +32,33 @@ function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
+      <SidebarInset className="overflow-hidden px-4 md:px-6 lg:px-8">
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+            <div className="flex flex-1 items-center gap-2 px-3">
+              <SidebarTrigger className="-ms-4" />
+              <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href={routes.admin.home}>
+                      <PiGauge size={22} aria-hidden="true" />
+                      <span className="sr-only">Dashboard</span>
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>Années Académiques</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+            <div className="flex gap-3 ml-auto">
+              <ModeToggle />
+              <UserDropdown />
+            </div>
+          </header>
+          {children}
+        </SidebarInset>
     </SidebarProvider>
   );
 }
